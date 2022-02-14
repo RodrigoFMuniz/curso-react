5b3eb7da2c054b312940aa583134d7d72119b8d8
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-02-13 21:43:46 -0300

    Estrutura reorganizada

--- src/Components/PostCard/index.jsx
+++ src/Components/PostCard/index.jsx
@@ -2,7 +2,7 @@ export const PostCard = ({post})=>{
   console.log(post.id)
   return (
     <div  className = "post-card">
-      <img src={post.cover} alt="{post.title}" className='image'/>
+      <img src={post.cover} alt={post.title}className='image'/>
       <h1>{post.id}- {post.title}</h1>
       <hr />
       <p>{post.body}</p>
